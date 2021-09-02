fe0c61220661f81cc30eca7f720e6f0dbce9a028
root <zyscal>
2021-09-02 09:46:02 +0800

    update 20210902

--- 15.三数之和/main.cpp
+++ 15.三数之和/main.cpp
@@ -1,16 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution
 	public:
 	vector<vector<int>> threeSum(vector<int>& nums) {
-				
+		vector<vector<int>> ans;
+
+		if(nums.size() < 3 ) 	
+
+
 	 }
-	vector<vector<int>> twoSum(vector<int>& nums) {
+	vector<vector<int>> twoSum(vector<int>& nums, int target) {
+		vector<vector<int>> ans;
 		if (nums.size() <= 1){
-			
+			return ans;
 		}	
+		auto iterBegin = nums.begin();
+		auto iterEnd = nums.end() - 1;
+		while(iterBegin < iterEnd) {
+			if(*iterBegin + *iterEnd == target){
+				vector<int> temAns = {*iterBegin, *iterEnd};
+				ans.push_back(temAns);
+				while (iterBegin < iterEnd - 1) {
+					if (*iterBegin == *(iterBegin + 1)) {
+						iterBegin++;
+					}
+					else{
+						break;
+					}
+				}
+				while (iterBegin < iterEnd - 1) {
+					if(*iterEnd == *(iterEnd - 1)){
+						iterEnd--;
+					}
+					else {
+						break;
+					}
+				}
+				iterBegin++;
+				iterEnd--;
+				
+			}
+			else if (*iterBegin + *iterEnd < target) {
+				iterBegin++;
+			}
+			else {
+				iterEnd--;
+			}
+		}
+		return ans;
+
 	}
 };
 int main() {
